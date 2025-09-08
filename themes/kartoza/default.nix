c6df1e5b61778d6dd182b19d5a370a9429743c52
Tim Sutton <tim@kartoza.com>
2025-09-08 19:05:27 +0100

    Fix theming of kartoza theme

--- themes/kartoza/default.nix
+++ themes/kartoza/default.nix
@@ -1,7 +1,7 @@
 { stdenvNoCC, ... }:
 stdenvNoCC.mkDerivation {
   pname = "kartoza-grub-theme";
-  version = "0.2.0";
+  version = "0.3.0";
 
   # If your theme files live in ./assets (theme.txt, icons/, etc.)
   src = ./assets;
